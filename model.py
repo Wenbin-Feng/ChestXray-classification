a0f88dfe50923b831b09202b9c8dee732deb175d
feng wenbin <145995896+Wenbin-Feng@users.noreply.github.com>
2025-08-28 11:59:10 +0800

    Update model.py

--- model.py
+++ model.py
@@ -27,30 +27,5 @@ class DieaseClassifier(nn.Module):
         return self.classifier(cls_token)
     
 
-# class CatDogClassifier(nn.Module):
-#     def __init__(self, num_classes=2, img_size=224, pretrained=True):
-#         super().__init__()
-#         # 使用timm库的VisionTransformer并加载预训练权重
-#         self.vit = VisionTransformer(
-#             img_size=img_size,
-#             patch_size=16,
-#             in_chans=3,
-#             embed_dim=768,
-#             depth=12,
-#             num_heads=12
-#         )
-        
-#         # 增强的分类头
-#         self.classifier = nn.Sequential(
-#             nn.LayerNorm(768),
-#             nn.Linear(768, 512),
-#             nn.GELU(),
-#             nn.Dropout(0.5),
-#             nn.Linear(512, num_classes)
-#         )
-        
-#     def forward(self, x):
-#         x = self.vit(x)  # 输出包含CLS token的特征
-#         cls_token = x[:, 0]  # 使用CLS token
-#         return self.classifier(cls_token)
     
+
